7e38bbefe93f260685c0673ae0012045ffb4dbcd
fanxiangyang <fanxiangyang_heda@163.com>
2015-11-04 22:23:04 +0800

    Initial Commit

---
+++ FanSuperiOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FanSuperiOS
+//
+//  Created by 向阳凡 on 15/11/4.
+//  Copyright © 2015年 向阳凡. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FanSuperiOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FanSuperiOS
+//
+//  Created by 向阳凡 on 15/11/4.
+//  Copyright © 2015年 向阳凡. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
